28bbd3f556279eb2e2932f7bea0ce636879ebc06
Volha Melayok <neiwra@gmail.com>
2024-07-10 13:55:49 +0200

    feat: add opportunity to create, update and delete employee

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,11 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
+import { Employee } from '@prisma/client';
 
 @Controller('employees')
 export class EmployeeController {
   constructor(private readonly employeeService: EmployeeService) {}
+
+  @Post()
+  createEmployee(@Body() data: Employee) {
+    return this.employeeService.createEmployee(data);
+  }
+
   @Get()
   getAllEmployees() {
     return this.employeeService.getAllEmployees();
   }
+
+  @Get(':id')
+  getEmployeeById(@Param('id') id: number) {
+    return this.employeeService.getEmployeeById(+id);
+  }
+
+  @Put(':id')
+  updateEmployee(@Param('id') id: number, @Body() data: Employee) {
+    return this.employeeService.updateEmployee(+id, data);
+  }
+
+  @Delete(':id')
+  deleteEmployee(@Param('id') id: number) {
+    return this.employeeService.deleteEmployee(+id);
+  }
+
+
 }
--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../services/prisma/prisma.service';
+import { Employee } from '@prisma/client';
 
 @Injectable()
 export class EmployeeService {
   constructor(private readonly prisma: PrismaService) {}
+
+  createEmployee(data: Employee) {
+    return this.prisma.employee.create({ data });
+  }
   getAllEmployees() {
     return this.prisma.employee.findMany();
+  }
+
+  getEmployeeById(id: number) {
+    return this.prisma.employee.findUnique({ where: { id } });
+  }
+
+  updateEmployee(id: number, data: Employee) {
+    return this.prisma.employee.update({ where: { id }, data });
+  }
+  deleteEmployee(id: number) {
+    return this.prisma.employee.delete({ where: { id } });
   }
 }
